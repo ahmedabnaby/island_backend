36a29bf40b590dbd942b29d67de732b891316f60
AhmedAbdelnaby <ahmedabnaby.97@gmail.com>
2022-02-05 16:06:53 +0200

    image_upload trial 1

--- island/models.py
+++ island/models.py
@@ -3,7 +3,11 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     title = models.CharField(max_length=120)
-    image = models.CharField(max_length=120)
+    # image = models.CharField(max_length=120)
+    image = models.ImageField(
+        upload_to='uploads/categories/',
+        max_length=254, blank=True, null=True
+    )
     description = models.TextField()
 
     def _str_(self):
--- island/views.py
+++ island/views.py
@@ -2,9 +2,21 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .serializers import CategorySerializer
 from .models import Category
+from rest_framework.response import Response
 
 # Create your views here.
 
 class CategoryView(viewsets.ModelViewSet):
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
+
+    def post(self, request, format=None):
+        serializer = CategorySerializer(data=request.data)
+
+        if serializer.is_valid():
+                serializer.save(
+                category=Category.objects.get(code=request.data.get('categories')),
+                image=request.data.get('image')
+            )
+        return Response(data=serializer.data)
+        # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
